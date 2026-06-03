37120e2100a8c08f119c1f26707bd8bb4b40d80b
shubhamsonaw <shubhamsonaw@gmail.com>
2026-06-04 00:18:02 +0530

    done serializers and views for chat API

--- apps/assistant/views.py
+++ apps/assistant/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
+from .serializers import ChatSerializer
+from .services import AssistantService
 # Create your views here.
+
+class AssistantChatView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        serializer = ChatSerializer(
+            data=request.data
+        )
+
+        serializer.is_valid(
+            raise_exception=True
+        )
+
+        response_text = AssistantService.chat(
+            request.user,
+            serializer.validated_data["message"]
+        )
+
+        return Response({
+            "response": response_text
+        })

a09d34ce82271be38f37a5275fdcc7bee93f6f94
shubhamsonaw <shubhamsonaw@gmail.com>
2026-06-03 16:12:44 +0530

    created ChatMessage model

--- apps/assistant/admin.py
+++ apps/assistant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import ChatMessage
 # Register your models here.
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "role",
+        "created_at"
+    )
--- apps/assistant/models.py
+++ apps/assistant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class ChatMessage(models.Model):
+
+    ROLE_CHOICES = (
+        ("user", "User"),
+        ("assistant", "Assistant"),
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.role} - {self.user.username}"

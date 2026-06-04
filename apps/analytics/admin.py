4fa0d9b71fdcfa04fdb7d72e417b929eb2b25c70
shubhamsonaw <shubhamsonaw@gmail.com>
2026-06-05 00:13:40 +0530

    add ContentAnalytics model in analytics

--- apps/analytics/admin.py
+++ apps/analytics/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import ContentAnalytics
 # Register your models here.
+
+@admin.register(ContentAnalytics)
+class ContentAnalyticsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "content_title",
+        "platform",
+        "impressions",
+        "likes",
+        "comments",
+        "shares",
+    )
--- apps/analytics/models.py
+++ apps/analytics/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class ContentAnalytics(models.Model):
+    platform = models.CharField(max_length=50)
+
+    content_title = models.CharField(max_length=255)
+
+    impressions = models.IntegerField(default=0)
+
+    likes = models.IntegerField(default=0)
+
+    comments = models.IntegerField(default=0)
+
+    shares = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content_title

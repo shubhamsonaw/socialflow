33a254ccc33cac40963d90cc4a3136cce69c0a0d
shubhamsonaw <shubhamsonaw@gmail.com>
2026-06-05 01:15:29 +0530

    integrate celery with redis

--- config/settings.py
+++ config/settings.py
@@ -149,3 +149,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"

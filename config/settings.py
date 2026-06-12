76bd15b166305f886cd58a1882e60d057d8b4ca0
shubhamsonaw <shubhamsonaw@gmail.com>
2026-06-13 01:35:17 +0530

    done things before docker commands

--- config/settings.py
+++ config/settings.py
@@ -155,9 +155,19 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"
+# CELERY_BROKER_URL = "redis://localhost:6379/0"
 
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+# CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+
+CELERY_BROKER_URL = os.getenv(
+    "CELERY_BROKER_URL",
+    "redis://localhost:6379/0"
+)
+
+CELERY_RESULT_BACKEND = os.getenv(
+    "CELERY_RESULT_BACKEND",
+    "redis://localhost:6379/0"
+)
 
 CELERY_BEAT_SCHEDULE = {
     "daily-analytics-report": {

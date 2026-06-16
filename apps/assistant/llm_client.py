f36b74b048a5c6b955fe75f984f1e0aaed1155c8
shubhamsonaw <shubhamsonaw@gmail.com>
2026-06-16 23:40:44 +0530

    add and update some files in assistant app

--- apps/assistant/llm_client.py
+++ apps/assistant/llm_client.py
@@ -1,11 +1,30 @@
-import google.generativeai as genai
+# import google.generativeai as genai
+
+# from django.conf import settings
+
+
+# genai.configure(
+#     api_key=settings.GEMINI_API_KEY
+# )
+
+
+# class GeminiClient:
+
+#     @staticmethod
+#     def generate_response(prompt):
+
+#         model = genai.GenerativeModel("gemini-2.5-flash")
+
+#         response = model.generate_content(
+#             prompt
+#         )
+
+#         return response.text
+
+
+from google import genai
 
 from django.conf import settings
-
-
-genai.configure(
-    api_key=settings.GEMINI_API_KEY
-)
 
 
 class GeminiClient:
@@ -13,10 +32,13 @@ class GeminiClient:
     @staticmethod
     def generate_response(prompt):
 
-        model = genai.GenerativeModel("gemini-2.5-flash")
+        client = genai.Client(
+            api_key=settings.GEMINI_API_KEY
+        )
 
-        response = model.generate_content(
-            prompt
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
         )
 
         return response.text
